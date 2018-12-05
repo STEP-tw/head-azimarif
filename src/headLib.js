81781dcd786ad3c4325fde11fdaf025b9746b30c
Arif <44020819+azimarif@users.noreply.github.com>
2018-12-05 07:16:38 +0530

    Add parseInput handling the default case of head

---
+++ src/headLib.js
@@ -0,0 +1,11 @@
+const parseInput = function(arguments) {
+  return {
+    type: 'n',
+    count: 10,
+    files: arguments
+  };
+}
+
+module.exports = {
+  parseInput
+}
---
+++ test/headLibTest.js
@@ -0,0 +1,12 @@
+const {
+  parseInput
+} = require('../src/headLib.js');
+const {
+  deepEqual
+} = require('assert');
+
+describe('parseInput', () => {
+  it('should return an object with type n, count 10 and fileName in files array while passing the fileName as input', () => {
+    deepEqual(parseInput(['file1.txt']), { type: 'n', count: 10, files: ['file1.txt'] });
+  });
+});

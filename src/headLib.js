7fdf1efad1ffba71cafb2e88d4d3fae72fc62325
Arif <44020819+azimarif@users.noreply.github.com>
2018-12-05 07:45:12 +0530

    Add getFirstNLines function

--- src/headLib.js
+++ src/headLib.js
@@ -6,10 +6,16 @@ const parseInput = function(arguments) {
   };
 }
 
+const getFirstNLines = function(fileContent, count) {
+  fileContent = fileContent.split('\n');
+  return fileContent.slice(0, count).join('\n');
+}
+
 const readFile = function(fs, file) {
   return fs.readFileSync(file, 'utf-8');
 }
 
 module.exports = {
   parseInput,
+  getFirstNLines
 }

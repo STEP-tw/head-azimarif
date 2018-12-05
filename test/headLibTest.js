801fa2f0a8af2096ba5732258727c5e41e72ea84
Arif <44020819+azimarif@users.noreply.github.com>
2018-12-05 09:40:18 +0530

    Add test cases for getFirstNLines

--- test/headLibTest.js
+++ test/headLibTest.js
@@ -1,5 +1,6 @@
 const {
-  parseInput
+  parseInput,
+  getFirstNLines
 } = require('../src/headLib.js');
 const {
   deepEqual
@@ -13,3 +14,17 @@ describe('parseInput', () => {
     deepEqual(parseInput(['file1.txt', 'file2.txt', 'file3.txt']), { type: 'n', count: 10, files: ['file1.txt', 'file2.txt', 'file3.txt'] });
   });
 });
+
+describe('getFirstNLines', () => {
+  const fileContent = 'One\nTwo\nThree\nFour\nFive\nSix\nSeven\nEight\nNine\nTen';
+  it('should return 0 line (empty string) when count is 0', () => {
+    deepEqual(getFirstNLines(fileContent, 0), '');
+  });
+  it('should return 1 line when count is 1', () => {
+    deepEqual(getFirstNLines(fileContent, 1), 'One');
+  });
+  it('should return 5 lines  when count is 5', () => {
+    deepEqual(getFirstNLines(fileContent, 5), 'One\nTwo\nThree\nFour\nFive');
+  });
+});
+

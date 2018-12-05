d124cca5c467c631923ac1f85f8b23d61fc00686
Arif <44020819+azimarif@users.noreply.github.com>
2018-12-05 07:18:48 +0530

    Add test case handling multiple file names as input in parseInput

--- test/headLibTest.js
+++ test/headLibTest.js
@@ -9,4 +9,7 @@ describe('parseInput', () => {
   it('should return an object with type n, count 10 and fileName in files array while passing the fileName as input', () => {
     deepEqual(parseInput(['file1.txt']), { type: 'n', count: 10, files: ['file1.txt'] });
   });
+  it('should return an object with type n, count 10 and fileNames in files array while passing multiple fileName as input', () => {
+    deepEqual(parseInput(['file1.txt', 'file2.txt', 'file3.txt']), { type: 'n', count: 10, files: ['file1.txt', 'file2.txt', 'file3.txt'] });
+  });
 });

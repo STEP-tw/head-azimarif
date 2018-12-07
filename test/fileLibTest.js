2253ac40c7f4efcb5bec1307f7a49dc19e7636a8
Arif <44020819+azimarif@users.noreply.github.com>
2018-12-07 14:24:10 +0530

    Add test cases for getFileDetails, implementing mock fs in test

--- test/fileLibTest.js
+++ test/fileLibTest.js
@@ -22,3 +22,39 @@ describe('fileNotFound', () => {
     deepEqual(fileNotFound('123.txt'), 'head: 123.txt: No such file or directory');
   });
 });
+
+const files = {
+  'file1': 'This is the content of file 1',
+  'file2': 'This is the content of file 2'
+}
+
+const fs = { 
+  existsSync : (file)=>{
+    if(files.hasOwnProperty(file)){
+      return true;
+    }
+    return false;
+  },
+  readFileSync : (file)=> {
+    return files[file];
+  }
+}
+
+describe('getFileDetails', () => {
+  it('should return the content of the file when file exists ', () => {
+    deepEqual(getFileDetails(fs, "file1"), {
+      content: 'This is the content of file 1',
+      name: 'file1',
+      isExists: true,
+      errorMessage: ''
+    });
+  });
+  it('should return the content of the file when file exists ', () => {
+    deepEqual(getFileDetails(fs, "file9"), {
+      content: undefined,
+      name: 'file9',
+      isExists: false,
+      errorMessage: 'head: file9: No such file or directory'
+    });
+  });
+});

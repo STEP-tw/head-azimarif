5e37784ebd61c9319a6c7b87dc19f4bf1791bf66
Arif <44020819+azimarif@users.noreply.github.com>
2018-12-11 10:10:23 +0530

    Add test cases for reverseText

--- test/utilTest.js
+++ test/utilTest.js
@@ -1,37 +1,47 @@
 const {
   isNumberGreater,
-  isValueNumber
-} = require('../src/util.js');
-const {
-  equal
-} = require('assert');
+  isValueNumber,
+  reverseText
+} = require("../src/util.js");
 
-describe('isNumberGreater', () => {
+const { equal } = require("assert");
+
+describe("isNumberGreater", () => {
   let isGreaterThanFive = isNumberGreater(5);
 
-  it('should return false when number is less than 5', () => {
+  it("should return false when number is less than 5", () => {
     equal(isGreaterThanFive(0), false);
   });
 
-  it('should return false when number is same itself', () => {
+  it("should return false when number is same itself", () => {
     equal(isGreaterThanFive(5), false);
   });
 
-  it('should return true when number is greater than 5', () => {
+  it("should return true when number is greater than 5", () => {
     equal(isGreaterThanFive(6), true);
   });
 });
 
-describe('isValueNumber', () => {
-  it('should return true when value is an int', () => {
+describe("isValueNumber", () => {
+  it("should return true when value is an int", () => {
     equal(isValueNumber(0), true);
   });
 
-  it('should return false when value is not an int', () => {
-    equal(isValueNumber('a'), false);
+  it("should return false when value is not an int", () => {
+    equal(isValueNumber("a"), false);
   });
 
-  it('should return false when value is some special character', () => {
-    equal(isValueNumber('-'), false);
+  it("should return false when value is some special character", () => {
+    equal(isValueNumber("-"), false);
+  });
+});
+
+describe("reverseText", () => {
+  it("should return empty string when input is empty", () => {
+    equal(reverseText(''), '');
+  });
+
+  it("should return string in reverse order", () => {
+    equal(reverseText('End'), 'dnE');
   });
 });

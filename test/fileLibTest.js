551f5bde90ebcb8b908163855d4c4827b8fc4c97
Arif <44020819+azimarif@users.noreply.github.com>
2018-12-12 11:07:23 +0530

    Add test cases for runTail

--- test/fileLibTest.js
+++ test/fileLibTest.js
@@ -5,7 +5,8 @@ const {
   getFileDetailsInReverse
 } = require('../src/fileLib.js');
 const {
-  runHead
+  runHead,
+  runTail
 } = require('../src/lib.js');
 const {
   deepEqual
@@ -123,6 +124,49 @@ describe('runHead', () => {
     });
     it('should return usage message when option other than n or c is given', () => {
       deepEqual(runHead(fs, ['-p', '0', 'file1']), 'head: illegal option -- p\nusage: head [-n lines | -c bytes] [file ...]');
+    });
+  });
+});
+
+
+describe('runTail', () => {
+  describe('with one file only', () => {
+    it('should return all lines from file when option & count is not given', () => {
+      deepEqual(runTail(fs, ['file1']), 'This is the content of file 1\nLine 2');
+    });
+    it('should return 1 line from file when option is n & line count is 1', () => {
+      deepEqual(runTail(fs, ['-n', '1', 'file1']), 'Line 2');
+    });
+    it('should return 1 byte from file when option is c & byte count is 1', () => {
+      deepEqual(runTail(fs, ['-c', '1', 'file1']), '2');
+    });
+    it('should return error message if file not found', () => {
+      deepEqual(runTail(fs, ['-c', '1', 'myFile']), 'tail: myFile: No such file or directory');
+    });
+
+  });
+
+  describe('with more than one file', () => {
+    it('should return all lines from file when option & count is not given', () => {
+      deepEqual(runTail(fs, ['file1', 'file2']), '==> file1 <==\nThis is the content of file 1\nLine 2\n\n==> file2 <==\nThis is the content of file 2');
+    });
+    it('should return 1 line from file when option is n & line count is 1', () => {
+      deepEqual(runTail(fs, ['-n', '1', 'file1', 'file2']), '==> file1 <==\nLine 2\n\n==> file2 <==\nThis is the content of file 2');
+    });
+    it('should return 1 byte from file when option is c & byte count is 1', () => {
+      deepEqual(runTail(fs, ['-c', '1', 'file1', 'file2']), '==> file1 <==\n2\n\n==> file2 <==\n2');
+    });
+    it('should return error message if file not found', () => {
+      deepEqual(runTail(fs, ['-c', '1', 'myFile', 'myFile2']), 'tail: myFile: No such file or directory\ntail: myFile2: No such file or directory');
+    });
+  });
+
+  describe('error message and usage', () => {
+    it('should return error message when count is less than 1 or not a number', () => {
+      deepEqual(runTail(fs, ['-n', '0', 'file1']), '');
+    });
+    it('should return usage message when option other than n or c is given', () => {
+      deepEqual(runTail(fs, ['-p', '0', 'file1']), 'tail: illegal option -- p\nusage: tail [-F | -f | -r] [-q] [-b # | -c # | -n #] [file ...]');
     });
   });
 });

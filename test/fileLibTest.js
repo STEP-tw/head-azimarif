f411c6699cdd9eb88fb96430b53322806d282d98
Arif <44020819+azimarif@users.noreply.github.com>
2018-12-17 15:34:37 +0530

    Extract expectedOutput of test cases in seperate variable

--- test/fileLibTest.js
+++ test/fileLibTest.js
@@ -9,71 +9,84 @@ const assert = require("assert");
 
 describe('getFileHeading', () => {
   it('should return the fileName with heading', () => {
-    let file = 'myFile.txt'
-    assert.deepEqual(getFileHeading(file), '==> '+ file +' <==\n');
+    let file = 'myFile.txt';
+    let expectedOutput = '==> ' + file + ' <==\n';
+    assert.equal(getFileHeading(file), expectedOutput);
   });
 });
 
 describe('displayFileNotFoundError', () => {
   it('should return file not found message with file name', () => {
-    assert.deepEqual(displayFileNotFoundError('myFile.txt', 'head'), 'head: myFile.txt: No such file or directory');
-    assert.deepEqual(displayFileNotFoundError('123.txt', 'head'), 'head: 123.txt: No such file or directory');
+    let expectedOutput = 'head: myFile.txt: No such file or directory';
+    assert.equal(displayFileNotFoundError('myFile.txt', 'head'), expectedOutput);
+
+    expectedOutput = 'head: 123.txt: No such file or directory';
+    assert.equal(displayFileNotFoundError('123.txt', 'head'), expectedOutput);
   });
 
   it('should return file not found message with file name', () => {
-    assert.deepEqual(displayFileNotFoundError('myFile.txt', 'tail'), 'tail: myFile.txt: No such file or directory');
-    assert.deepEqual(displayFileNotFoundError('123.txt', 'tail'), 'tail: 123.txt: No such file or directory');
+    let expectedOutput = 'tail: myFile.txt: No such file or directory';
+    assert.equal(displayFileNotFoundError('myFile.txt', 'tail'), expectedOutput);
+
+    expectedOutput = 'tail: 123.txt: No such file or directory';
+    assert.equal(displayFileNotFoundError('123.txt', 'tail'), expectedOutput);
   });
 });
 
 const files = {
-  'file1': 'This is the content of file 1\nLine 2',
-  'file2': 'This is the content of file 2'
+  file1: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].join('\n'),
+  file2: 'This is the content of file 2'
 }
 
-const fs = { 
-  existsSync : (file)=>{
+const fs = {
+  existsSync: (file) => {
     return files.hasOwnProperty(file);
   },
-  readFileSync : (file)=> {
+  readFileSync: (file) => {
     return files[file];
   }
 }
 
 describe('getFileDetails', () => {
   it('should return the content of the file when file exists ', () => {
-    assert.deepEqual(getFileDetails("file1", fs), {
-      content: 'This is the content of file 1\nLine 2',
+    let expectedOutput = {
+      content: '1\n2\n3\n4\n5\n6\n7\n8\n9\n10',
       name: 'file1',
       isExists: true,
       errorMessage: ''
-    });
+    };
+    assert.deepEqual(getFileDetails("file1", fs), expectedOutput);
   });
+
   it('should return the error message if the file does not exist ', () => {
-    assert.deepEqual(getFileDetails("file9", fs), {
+    let expectedOutput = {
       content: undefined,
       name: 'file9',
       isExists: false,
       errorMessage: 'head: file9: No such file or directory'
-    });
+    };
+    assert.deepEqual(getFileDetails("file9", fs), expectedOutput);
   });
 });
 
 describe('getFileDetailsInReverse', () => {
   it('should return the content of the file when file exists ', () => {
-    assert.deepEqual(getFileDetailsInReverse("file1", fs), {
-      content: '2 eniL\n1 elif fo tnetnoc eht si sihT',
+    let expectedOutput = {
+      content: '01\n9\n8\n7\n6\n5\n4\n3\n2\n1',
       name: 'file1',
       isExists: true,
       errorMessage: ''
-    });
+    };
+    assert.deepEqual(getFileDetailsInReverse("file1", fs), expectedOutput);
   });
+
   it('should return the error message if the file does not exist ', () => {
-    assert.deepEqual(getFileDetailsInReverse("file9", fs), {
+    let expectedOutput = {
       content: undefined,
       name: 'file9',
       isExists: false,
       errorMessage: 'tail: file9: No such file or directory'
-    });
+    };
+    assert.deepEqual(getFileDetailsInReverse("file9", fs), expectedOutput);
   });
 });

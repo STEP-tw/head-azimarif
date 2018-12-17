39cf457a0643b878fa2fcd380571f1066295f3d1
Arif <44020819+azimarif@users.noreply.github.com>
2018-12-17 16:37:42 +0530

    Removed unnecessary test cases

--- test/inputLibTest.js
+++ test/inputLibTest.js
@@ -1,54 +1,103 @@
-const {
-  parseInput,
-  getOptionDetails
-} = require('../src/inputLib.js');
-const {
-  deepEqual
-} = require('assert');
+const { parseInput } = require("../src/inputLib.js");
+const assert = require("assert");
 
-describe('parseInput', () => {
-  it('should return an object with type n, count 10 and fileName in files array while passing the fileName as input', () => {
-    deepEqual(parseInput(['file1.txt']), { type: 'n', count: 10, files: ['file1.txt'] });
+describe("parseInput", () => {
+  it("should return object with type n, count 10 and fileName in files array when only file name is passed as input", () => {
+    let expectedOutput = {
+      type: "n",
+      count: 10,
+      files: ["file1.txt"]
+    };
+    assert.deepEqual(parseInput(["file1.txt"]), expectedOutput);
   });
 
-  it('should return an object with type n, count 10 and fileNames in files array while passing multiple fileName as input', () => {
-    deepEqual(parseInput(['file1.txt', 'file2.txt', 'file3.txt']), { type: 'n', count: 10, files: ['file1.txt', 'file2.txt', 'file3.txt'] });
+  it("should return object with type n, count 10 and fileName in files array when multiple file name is passed as input", () => {
+    let expectedOutput = {
+      type: "n",
+      count: 10,
+      files: ["file1.txt", "file2.txt", "file3.txt"]
+    };
+    assert.deepEqual(parseInput(["file1.txt", "file2.txt", "file3.txt"]), expectedOutput);
   });
 
-  it('should return an object with type n and count, fileName in files array while passing the count & fileName as input', () => {
-    deepEqual(parseInput(['-0', 'file1.txt']), { type: 'n', count: 0, files: ['file1.txt'] });
-    deepEqual(parseInput(['-1', 'file1.txt', 'file2.txt', 'file3.txt']), { type: 'n', count: 1, files: ['file1.txt', 'file2.txt', 'file3.txt'] });
-    deepEqual(parseInput(['-100', 'file1.txt', 'file2.txt', 'file3.txt']), { type: 'n', count: 100, files: ['file1.txt', 'file2.txt', 'file3.txt'] });
+  it("should return an object with type n and count, fileName in files array while passing the count & fileName as input", () => {
+    let expectedOutput = {
+      type: "n",
+      count: 0,
+      files: ["file1.txt"]
+    };
+    assert.deepEqual(parseInput(["-0", "file1.txt"]), expectedOutput);
+
+    expectedOutput = {
+      type: "n",
+      count: 1,
+      files: ["file1.txt", "file2.txt", "file3.txt"]
+    };
+    assert.deepEqual(parseInput(["-1", "file1.txt", "file2.txt", "file3.txt"]), expectedOutput);
+
+    expectedOutput = {
+      type: "n",
+      count: 100,
+      files: ["file1.txt", "file2.txt", "file3.txt"]
+    };
+    assert.deepEqual(parseInput(["-100", "file1.txt", "file2.txt", "file3.txt"]), expectedOutput);
   });
 
-  it('should return an object of type, count and fileNames when all three arguments are passed', () => {
-    deepEqual(parseInput(['-n1', 'file1.txt']), { type: 'n', count: 1, files: ['file1.txt'] });
-    deepEqual(parseInput(['-n1', 'file1.txt', 'file2.txt']), { type: 'n', count: 1, files: ['file1.txt', 'file2.txt'] });
-    deepEqual(parseInput(['-n', '1', 'file1.txt']), { type: 'n', count: 1, files: ['file1.txt'] });
-    deepEqual(parseInput(['-n', '1', 'file1.txt', 'file2.txt']), { type: 'n', count: 1, files: ['file1.txt', 'file2.txt'] });
-    deepEqual(parseInput(['-c1', 'file1.txt']), { type: 'c', count: 1, files: ['file1.txt'] });
-    deepEqual(parseInput(['-c1', 'file1.txt', 'file2.txt']), { type: 'c', count: 1, files: ['file1.txt', 'file2.txt'] });
-    deepEqual(parseInput(['-c', '1', 'file1.txt']), { type: 'c', count: 1, files: ['file1.txt'] });
-    deepEqual(parseInput(['-c', '1', 'file1.txt', 'file2.txt']), { type: 'c', count: 1, files: ['file1.txt', 'file2.txt'] });
-  });
-});
+  it("should return an object of type, count and fileNames when all three arguments are passed", () => {
+    let expectedOutput = {
+      type: "n",
+      count: 1,
+      files: ["file1.txt"]
+    };
+    assert.deepEqual(parseInput(["-n1", "file1.txt"]), expectedOutput);
 
-describe('getOptionDetails', () => {
-  it('n option', () => {
-    it('should return an object of type, count and fileNames when all three arguments are passed', () => {
-      deepEqual(getOptionDetails(['-n1', 'file1.txt']), { type: 'n', count: 1, files: ['file1.txt'] });
-      deepEqual(getOptionDetails(['-n1', 'file1.txt', 'file2.txt']), { type: 'n', count: 1, files: ['file1.txt', 'file2.txt'] });
-      deepEqual(getOptionDetails(['-n', '1', 'file1.txt']), { type: 'n', count: 1, files: ['file1.txt'] });
-      deepEqual(getOptionDetails(['-n', '1', 'file1.txt', 'file2.txt']), { type: 'n', count: 1, files: ['file1.txt', 'file2.txt'] });
-    });
-  });
-  it('c option', () => {
-    it('should return an object of type, count and fileNames when all three arguments are passed', () => {
+    expectedOutput = {
+      type: "n",
+      count: 1,
+      files: ["file1.txt", "file2.txt"]
+    };
+    assert.deepEqual(parseInput(["-n1", "file1.txt", "file2.txt"]), expectedOutput);
 
-      deepEqual(getOptionDetails(['-c1', 'file1.txt']), { type: 'c', count: 1, files: ['file1.txt'] });
-      deepEqual(getOptionDetails(['-c1', 'file1.txt', 'file2.txt']), { type: 'c', count: 1, files: ['file1.txt', 'file2.txt'] });
-      deepEqual(getOptionDetails(['-c', '1', 'file1.txt']), { type: 'c', count: 1, files: ['file1.txt'] });
-      deepEqual(getOptionDetails(['-c', '1', 'file1.txt', 'file2.txt']), { type: 'c', count: 1, files: ['file1.txt', 'file2.txt'] });
-    });
+    expectedOutput = {
+      type: "n",
+      count: 1,
+      files: ["file1.txt"]
+    };
+    assert.deepEqual(parseInput(["-n", "1", "file1.txt"]), expectedOutput);
+
+    expectedOutput = {
+      type: "n",
+      count: 1,
+      files: ["file1.txt", "file2.txt"]
+    };
+    assert.deepEqual(parseInput(["-n", "1", "file1.txt", "file2.txt"]), expectedOutput);
+
+    expectedOutput = {
+      type: "c",
+      count: 1,
+      files: ["file1.txt"]
+    };
+    assert.deepEqual(parseInput(["-c1", "file1.txt"]), expectedOutput);
+
+    expectedOutput = {
+      type: "c",
+      count: 1,
+      files: ["file1.txt", "file2.txt"]
+    };
+    assert.deepEqual(parseInput(["-c1", "file1.txt", "file2.txt"]),expectedOutput);
+
+    expectedOutput = {
+      type: "c",
+      count: 1,
+      files: ["file1.txt"]
+    };
+    assert.deepEqual(parseInput(["-c", "1", "file1.txt"]), expectedOutput);
+
+    expectedOutput = {
+      type: "c",
+      count: 1,
+      files: ["file1.txt", "file2.txt"]
+    };
+    assert.deepEqual(parseInput(["-c", "1", "file1.txt", "file2.txt"]), expectedOutput);
   });
 });
